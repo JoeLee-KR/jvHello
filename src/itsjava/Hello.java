a6d7b088d62e27cbeb90ba4d38528d6b4b5547e3
DoogieMac <xjoelee@gmail.com>
2025-11-11 16:45:19 +0900

    joeMac:251111-1645:

--- src/itsjava/Hello.java
+++
@@ -1,7 +0,0 @@
-package itsjava;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, Java...");
-    } // main
-} // Hello
---
+++ src/itsjava/ch01/Hello.java
@@ -0,0 +1,7 @@
+package itsjava.ch01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java... itsjava.ch01.Hello...at Artifacts");
+    }
+}
