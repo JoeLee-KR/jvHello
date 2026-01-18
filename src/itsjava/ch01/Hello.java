a153109ec7cf3b68339717d0f252ee4bd1940e68
doogie <doogie@emma.local>
2026-01-18 22:30:53 +0900

    emma:260118-2230

--- src/itsjava/ch01/Hello.java
+++ src/itsjava/ch01/Hello.java
@@ -2,12 +2,6 @@ package itsjava.ch01;
 
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, Java...");
-    } // main
-} // Hello
-=======
         System.out.println("Hello, Java... itsjava.ch01.Hello...at Artifacts");
     }
 }
->>>>>>> 3f89b5ee7e55bc1d5fb79ff7e990e42adb997b4c
